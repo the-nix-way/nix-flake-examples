c5bd2b844bbfd280411a02a6f24a9a62671a020e
Luc Perkins <lucperkins@gmail.com>
2022-06-20 22:05:14 +0200

    Add some basic inline documentation

--- hello/flake.nix
+++ hello/flake.nix
@@ -1,21 +1,28 @@
 {
   description = "A flake with just the hello package";
 
+  # GitHub URLs for the Nix inputs we're using
   inputs = {
+    # Good old nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs";
+    # flake-utils is a set of helper functions for using flakes
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
+    # Recurse through systems supported by flake-utils
     flake-utils.lib.eachDefaultSystem
       (system:
+        # Use a convenience variable for all nixpkgs packages for the specified system
         let pkgs = nixpkgs.legacyPackages.${system}; in
         {
           packages = with pkgs; {
+            # Import the hello package
             inherit hello;
-          };
 
-          defaultPackage = pkgs.hello;
+            # Set the default package
+            default = hello;
+          };
         }
       );
 }
